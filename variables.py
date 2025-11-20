857b3b27cc643ee90bc3023c94d4bcdd454174ae
DGaro <catlynsmith036@gmail.com>
2025-11-20 14:23:15 +0300

    conditional_statements

--- variables.py
+++ variables.py
@@ -1,6 +1,23 @@
+# variables
 name = 'Denick'
 print(name)
 a = 10
 b = 3.14
 print(a)
+is_student = True 
+print("Am I a student?", is_student)
+print(is_student)
+x = None
+print(x)
+print(type(x))
+m = 5
+n = 8 
+print(m)
+print(n)
+
+m =8
+print(m)
+
+# conditional_statements
+
 
